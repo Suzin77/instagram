c4a8e3f960ed407b1305b0dc8f78b972b803c20e
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-01 22:35:19 +0100

    add user info and image list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,46 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle p-3" src="/img/green-profile.png" style="max-height: 200px">
+        </div>
+        <div class="col-9 p-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>153</strong> posts</div>
+                <div class="pr-4"><strong>23k</strong> followers</div>
+                <div class="pr-4"><strong>212</strong> follows</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCloseOrg</div>
+            <div>
+                Lorem ipsum – tekst składający się z łacińskich i quasi-łacińskich wyrazów, mający korzenie w klasycznej łacinie,
+                wzorowany na fragmencie traktatu Cycerona „O granicach dobra i zła” (De finibus bonorum et malorum)
+                napisanego w 45 p.n.e. Tekst jest stosowany do demonstracji krojów pisma (czcionek, fontów), kompozycji kolumny itp.
+                Po raz pierwszy został użyty przez nieznanego drukarza w XVI wieku.
+                Tekst w obcym języku pozwala skoncentrować uwagę na wizualnych aspektach tekstu, a nie jego znaczeniu.
+                W łacinie niektóre z często występujących w językach nowożytnych liter używane były sporadycznie (k, z),
+                bądź nie było ich wcale (w), stąd niektóre wariacje tego tekstu mogą zawierać także wyrazy z tymi literami,
+                aby również i one były zademonstrowane.
+                Fragment używany do demonstracji tekstu:lorem lorem  lorem
+            </div>
+            <div><a href="https://lin-q.pl" target="_blank">lin-q.pl</a></div>
         </div>
     </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1526228446530-49914082ff8a?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1526228446530-49914082ff8a?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1526228446530-49914082ff8a?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" class="w-100">
+        </div>
+
+
+    </div>
+
 </div>
 @endsection
