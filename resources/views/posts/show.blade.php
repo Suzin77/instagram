3412d3710fcffaf76290a048bba9301fc862ca7c
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-03 22:04:05 +0100

    add show post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,10 +4,18 @@
     <div class="container">
         <div class="row">
             <div class="col-8">
-                <img src="/storage/{{$post->image}}" alt="" class="">
+                <img src="/storage/{{$post->image}}" alt="" class="w-100">
             </div>
             <div class="col-4">
-                <h3>{{$post->user->username}}</h3>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="pr-3">
+                        <img class="rounded-circle w-100" src="/storage/{{$post->user->profile->image}}" alt="" style="max-width: 50px">
+                    </div>
+
+                    <div>
+                        <h2>{{$post->user->username}}</h2>
+                    </div>
+                </div>
                 <p>{{$post->caption}}</p>
             </div>
         </div>
