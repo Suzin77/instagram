99bf4c7e7305cc5489f79bfe1fe5b1f1b8aa2028
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-03 07:21:17 +0100

    update view of user profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,11 @@
         <div class="col-9 p-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="">Add new Post</a>
+                <a href="/p/create">Add new Post</a>
             </div>
+            <div class="" style="border-bottom: 1px solid #1b1e21"></div>
             <div class="d-flex">
-                <div class="pr-4"><strong>153</strong> posts</div>
+                <div class="pr-4"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-4"><strong>23k</strong> followers</div>
                 <div class="pr-4"><strong>212</strong> follows</div>
             </div>
@@ -25,16 +26,15 @@
     </div>
 
     <div class="row pt-4">
-        <div class="col-4">
-            <img src="https://images.unsplash.com/photo-1526228446530-49914082ff8a?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://images.unsplash.com/photo-1526228446530-49914082ff8a?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://images.unsplash.com/photo-1526228446530-49914082ff8a?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" class="w-100">
-        </div>
 
+        @foreach($user->posts as $post)
+
+        <div class="col-4 pt-4">
+            <a href="/p/{{$post->id}}">
+                <img src="/storage/{{$post->image}}" alt="" class="w-100">
+            </a>
+        </div>
+        @endforeach
 
     </div>
 
