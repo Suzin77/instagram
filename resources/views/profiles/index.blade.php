b0419b40c8be1f5ddec086798f946b87fe623d51
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-03 22:03:31 +0100

    add default image to profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,14 +4,28 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img class="rounded-circle p-3" src="/img/green-profile.png" style="max-height: 200px">
+{{--            <img class="rounded-circle p-3" src="{{"/storage/".$user->profile->image ?? "img/default-profile.png"}}" style="max-height: 200px">--}}
+            @if($user->profile->image)
+                <img class="rounded-circle p-3 w-100" src="/storage/{{$user->profile->image }}">
+            @else
+                <img class="rounded-circle p-3 w-100" src="/img/default-profile.png" >
+            @endif
+
         </div>
         <div class="col-9 p-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
+            @can('update',$user->profile)
                 <a href="/p/create">Add new Post</a>
+            @endcan
             </div>
             <div class="" style="border-bottom: 1px solid #1b1e21"></div>
+            <div class="pt-1">
+                @can('update',$user->profile)
+                    <a href="/profile/{{$user->id}}/edit">Edit profile</a>
+                @endcan
+            </div>
+
             <div class="d-flex">
                 <div class="pr-4"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-4"><strong>23k</strong> followers</div>
