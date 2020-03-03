5dfb97ede4beaac4d2e81ef70c142bff709cccd5
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-03 22:04:47 +0100

    add boot method for user to create profile on creating user

--- app/User.php
+++ app/User.php
@@ -37,6 +37,19 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(
+            function ($user){
+                $user->profile()->create([
+                    'title' => 'default profile title',
+                    'image' => ''
+                ]);
+            }
+        );
+    }
+
     public function profile()
     {
         return $this->hasOne(Profile::class);
