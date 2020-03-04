6cd96d941e1bb20b02993d8f43f03cd01520a423
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-04 13:19:26 +0100

    add vue follow button

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,7 +10,7 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-2">
                     <div class="h3">{{$user->username}}</div>
-                    <button class="btn btn-primary btn-sm ml-2">Follow</button>
+                    <follow-button></follow-button>
                 </div>
                 @can('update',$user->profile)
                     <a href="/p/create">Add new Post</a>
