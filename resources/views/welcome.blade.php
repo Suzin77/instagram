db742800e61f185ad8ddb6c4609a6797bf881ae5
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-04 20:01:40 +0100

    add laravel\telescope

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+                    <a href="/profile/1"> Profile 1</a>
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
